005727ab86637b309a73641b9621799023397ade
jaime junior <yajaxi5175@gosarlar.com>
2025-06-08 20:55:46 -0300

    test: adding report

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter', // Adiciona o repórter
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Relatório de Testes - Meu Projeto',
+    embeddedScreenshots: true,
+    inlineAssets: true, // Para ter um arquivo HTML único
+    saveAllAttempts: false,
+  },
   e2e: {
     baseUrl: "https://qa-practice.netlify.app/",
     setupNodeEvents(on, config) {
